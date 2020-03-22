ee84286f95f77e8479bad3190948670ded757f2c
Guillem Gelabert <44653501+guillem-gelabert@users.noreply.github.com>
2020-03-22 11:20:41 +0100

    t fix tests by injecting a mock provider

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { USER_MODEL } from './constants';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('Users Controller', () => {
   let controller: UsersController;
@@ -7,6 +10,13 @@ describe('Users Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(USER_MODEL),
+          useValue: { username: '', email: '' },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { getModelToken } from '@nestjs/mongoose';
+import { USER_MODEL } from './constants';
+
+const userModel = {
+  username: 'mock_user',
+  email: 'mock_mail@mock_provider.com',
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [],
+      controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(USER_MODEL),
+          useValue: userModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);

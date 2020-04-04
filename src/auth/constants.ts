ff53148fc344f53960d6b676342482b89e33de19
Guillem Gelabert <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-04 10:21:28 +0200

    F force jwt secret to env variable to avoid accidentally going to production with fallback secret

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,9 @@
+export const PROD = 'PROD';
+export const DEV = 'DEV';
+
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET || 'very_secret_secret_key', // FIXME: set env config
+  secret:
+    process.env.NODE_ENV === DEV
+      ? 'very_secret_secret_key'
+      : process.env.JWT_SECRET,
 };

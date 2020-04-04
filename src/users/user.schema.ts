3cff2749d5f7b8848afc351db052294e62d892ef
Guillem Gelabert <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-04 18:00:31 +0200

    F throw an error if email and username are not unique

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,7 +1,20 @@
 import { Schema } from 'mongoose';
 
 export const UserSchema = new Schema({
-  email: String,
-  hash: String,
-  username: String,
+  email: {
+    required: true,
+    set: (v: string): string => v.toLowerCase(),
+    type: String,
+    unique: true,
+  },
+  hash: {
+    required: true,
+    type: String,
+  },
+  username: {
+    required: true,
+    set: (v: string): string => v.toLowerCase(),
+    type: String,
+    unique: true,
+  },
 });

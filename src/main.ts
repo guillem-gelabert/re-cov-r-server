c3ae7ab72547708fb0611685fe9e290d3ac0b5c9
Guillem Gelabert <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-07 17:30:29 +0200

    F setup field validation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { PORT } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   await app.listen(configService.get(PORT) || 3000);
 }

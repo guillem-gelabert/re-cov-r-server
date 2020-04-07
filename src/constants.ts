bbd1f937282f7125c6f5389ba1b3bd79a403ffa2
Guillem Gelabert <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-07 13:38:10 +0200

    F add env strings to constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const PROD = 'PROD';
+export const DEV = 'DEV';
+export const JWT_SECRET = 'JWT_SECRET';
+export const MONGO_CONNECTION_STRING = 'MONGO_CONNECTION_STRING';
+export const PORT = 'PORT';
